1efb21ef1e55465ca2e7ba4a77537e4be608c32e
harry <hhr_harry@qq.com>
2018-10-29 22:20:09 +0800

    update

--- src/main/java/cn/harryai/test/helloworld/controller/HelloWorldController.java
+++ src/main/java/cn/harryai/test/helloworld/controller/HelloWorldController.java
@@ -1,13 +1,46 @@
 package cn.harryai.test.helloworld.controller;
 
+import cn.harryai.test.helloworld.service.DBService;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class HelloWorldController {
+    @Autowired
+    DBService dbService;
+
     @GetMapping("hello")
-    public String hello(){
+    public String hello() {
 
         return "hello-world";
     }
+
+    @GetMapping("get")
+    @ResponseBody
+    public String get() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "harry");
+        map.put("age", 12);
+        map.put("sex", 87);
+        System.out.println(JSON.toJSONString(map));
+
+        return JSONArray.toJSONString(map);
+    }
+
+    @GetMapping("get2")
+    @ResponseBody
+    public String get2(String s) {
+        System.out.println(s);
+        return dbService.findByKey(s).toString();
+
+    }
+
+
 }
